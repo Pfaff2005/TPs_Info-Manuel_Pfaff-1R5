f09cba262586380d6e5b39c86a0cd95bfff31909
Pfaff2005 <manu.neuquen2005@gmail.com>
2025-05-18 19:48:38 -0300

    Primer commit

---
+++ bmi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float peso, altura, imc;
+
+    printf("Ingrese el peso en kg: ");
+    scanf("%f", &peso);
+
+    printf("Ingrese la altura en metros: ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    printf("\nSu índice de masa corporal es: %.2f\n\n", imc);
+    printf("Índice | Condición\n");
+    printf("-----------------------------\n");
+    printf("<18.5  | Bajo peso\n");
+    printf("18.5 a 24.9 | Normal\n");
+    printf("25.0 a 29.9 | Sobrepeso\n");
+    printf(">=30   | Obesidad\n");
+
+
+}
