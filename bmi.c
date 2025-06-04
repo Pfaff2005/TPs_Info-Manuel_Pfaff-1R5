292fe993a99b4c0fff8d8ade049340b7c08a1b7e
Pfaff2005 <manu.neuquen2005@gmail.com>
2025-06-04 17:26:36 -0300

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -11,13 +11,25 @@ int main() {
 
     imc = peso / (altura * altura);
 
-    printf("\nSu índice de masa corporal es: %.2f\n\n", imc);
+   
     printf("Índice | Condición\n");
     printf("-----------------------------\n");
     printf("<18.5  | Bajo peso\n");
     printf("18.5 a 24.9 | Normal\n");
     printf("25.0 a 29.9 | Sobrepeso\n");
-    printf(">=30   | Obesidad\n");
+    printf(">=30   | Obesidad\n\n");
 
+    printf("\nSu índice de masa corporal es: %.2f\n", imc);
 
+    if (imc < 18.5) {
+        printf("Condición: Bajo peso\n");
+    } else if (imc >= 18.5 && imc < 25) {
+        printf("Condición: Normal\n");
+    } else if (imc >= 25 && imc < 30) {
+        printf("Condición: Sobrepeso\n");
+    } else {
+        printf("Condición: Obesidad\n");
+    }
+
+    return 0;
 }
